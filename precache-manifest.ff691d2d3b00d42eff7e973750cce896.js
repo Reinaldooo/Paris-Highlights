43c4852ee71eacfa8f3daba1d25192a41e8c97aa
Reinaldo Trindade <rewifetri@gmail.com>
2018-10-22 19:25:50 -0300

    Updates

---
+++ precache-manifest.ff691d2d3b00d42eff7e973750cce896.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13524623f590c20fcabd",
+    "url": "/paris-highlights/static/js/runtime~main.13524623.js"
+  },
+  {
+    "revision": "1dbd5ed95df03902b589",
+    "url": "/paris-highlights/static/js/main.1dbd5ed9.chunk.js"
+  },
+  {
+    "revision": "580b650ad2ad61e780b7",
+    "url": "/paris-highlights/static/js/1.580b650a.chunk.js"
+  },
+  {
+    "revision": "1dbd5ed95df03902b589",
+    "url": "/paris-highlights/static/css/main.0f5fbb59.chunk.css"
+  },
+  {
+    "revision": "5e1b507255218497c9ca3988796c0514",
+    "url": "/paris-highlights/index.html"
+  }
+];
